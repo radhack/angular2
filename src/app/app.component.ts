76df094f0c3a6883e982c549f2375590e9644946
Alex Griffen <radhack242@gmail.com>
2018-02-15 12:22:28 -0800

    Add hellosign.service.ts and update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HellosignService} from '../services/hellosign.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'It\'s Up and Stuff!';
+
+export class AppComponent implements OnInit{
+
+ public title = 'It\'s Up and Stuff!';
+
+  constructor(private hellosignService: HellosignService) {
+    this.hellosignService.startHelloSign( "d7219512693825facdd9241f458decf2", "https://app.hellosign.com/editor/embeddedSign?signature_id=a2084ce5fa2edc1e780e4efd5490f6de&token=6ebdc154daa90a8bead927ec42fb246e");
+  }
+
+  public ngOnInit() {
+  }
 }
---
+++ src/services/hellosign.service.ts
@@ -0,0 +1,68 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+
+declare var HelloSign: any;
+
+
+@Injectable()
+export class HellosignService {
+  public startHelloSign(clientId: string, url: string) {
+
+   const sub = Observable
+            .timer(250)
+            .filter(() => HelloSign)
+            .subscribe(() => {
+                console.info('RUNNING');
+                this.runHelloSign(clientId, url);
+                sub.unsubscribe();
+            });
+  }
+
+  private runHelloSign(clientId: string, url: string) {
+    HelloSign.init(clientId);
+    HelloSign.open({
+      url: url,
+      allowCancel: true,
+      skipDomainVerification: true,
+      uxVersion: 2,
+      debug: true,
+      messageListener: function (eventData) {
+        (console.log(">-*>-*>-*> Got message data: " + JSON.stringify(eventData)));
+        if (eventData.event == HelloSign.EVENT_SIGNED) {
+          if (eventData.signature_id == null) {
+            alert("SIGNATURE_ID MISSING");
+          }
+          HelloSign.close();
+          console.log(eventData.signature_id + " here's some information that's useful...*&^^&**&^^&**&^^&**&^^&*");
+        } else if (eventData.event == HelloSign.EVENT_CANCELED) {
+          HelloSign.close();
+          // alert("Event Canceled And Stuff!");
+          console.log(eventData);
+        } else if (eventData.event == HelloSign.EVENT_ERROR) {
+          // HelloSign.close();
+          // alert("There Was An Error And Stuff!");
+          console.log(eventData);
+        } else if (eventData.event == HelloSign.EVENT_SENT) {
+          if (eventData.signature_request_id == null) {
+            alert("SIGNATURE_REQUEST_ID MISSING");
+          }
+          HelloSign.close();
+          // alert("Signature Request Sent And Stuff!");
+          console.log(eventData);
+          console.log("************");
+          console.log(eventData.signature_request_id);
+        } else if (eventData.event == HelloSign.EVENT_TEMPLATE_CREATED) {
+          HelloSign.close();
+          var template_id = eventData.template_id;
+          // window.alert("Template ID is " + template_id);
+          console.log(eventData);
+        } else if (eventData.event == HelloSign.EVENT_DECLINED) {
+          HelloSign.close();
+          // alert("Signature Request declined And Stuff!");
+          console.log(eventData);
+        }
+      }
+    });
+  }
+
+}
