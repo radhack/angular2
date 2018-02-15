49b252d086255edc18db54eb94cca6ecb6ce9c2e
Alex Griffen <radhack242@gmail.com>
2018-02-15 12:21:03 -0800

    Add HelloSignService as providers to NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {HellosignService} from '../services/hellosign.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [HellosignService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
